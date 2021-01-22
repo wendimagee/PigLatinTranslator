addf3ef377f2fad8c0439ffea649ca0d93eb9f45
Wendi Magee <wendimagee@gmail.com>
2021-01-22 13:29:31 -0500

    You can translate a whole sentence now! However, we are throwing an exception for words like "my, fly, why, etc)

--- PigLatinTranslator/Program.cs
+++ PigLatinTranslator/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Linq;
-
+//this code works for every word except those that end in y and have no other consonants.
+//my apologies! I am learning. You can enter a whole sentence though!
 namespace PigLatinTranslator
 {
     class Program
@@ -14,45 +15,51 @@ namespace PigLatinTranslator
 
 
                 Console.WriteLine("Hello and Welcome to the Pig Latin Translator!");
-                Console.WriteLine("\nPlease enter a word: ");
+                Console.WriteLine("\nPlease enter a word or sentence to translate: ");
                 string input = Console.ReadLine();
-
-                // make an array of vowels to search for
-                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
-
-                //test input for vowels, find out where they are
-                int vowelPlace = input.IndexOfAny(vowels);
-
-                if (vowelPlace == 0)
+                //split those words up!
+                string[] words = input.Split(' ');
+                foreach (var word in words)
                 {
-                    Console.WriteLine(input + "way");
-                }
-                //if the word doesn't begin with a vowel
-                else if (vowelPlace == 1)
-                {
+                    Console.WriteLine(word);
 
-                    string split1 = input.Substring(0, vowelPlace);
-                    string split2 = input.Substring(vowelPlace);
-                    Console.WriteLine(split2 + split1 + "ay");
-                }
-                else
-                {
-                    string split1 = input.Substring(0, vowelPlace);
-                    string split2 = input.Substring(vowelPlace);
-                    Console.WriteLine(split2 + split1 + "ay");
-                }
+                    // make an array of vowels to search for
+                    char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
-                Console.WriteLine("Would you like to translate another word?(y/n)");
-                char answer = char.Parse(Console.ReadLine());
-                if (char.ToLower(answer) == 'y')
-                {
-                    goAgain = true;
+                    //test input for vowels, find out where they are
+                    int vowelPlace = word.IndexOfAny(vowels);
+
+                    if (vowelPlace == 0)
+                    {
+                        Console.WriteLine(word + "way");
+                    }
+                    //if the word doesn't begin with a vowel
+                    else if (vowelPlace == 1)
+                    {
+
+                        string split1 = word.Substring(0, vowelPlace);
+                        string split2 = word.Substring(vowelPlace);
+                        Console.WriteLine(split2 + split1 + "ay");
+                    }
+                    else
+                    {
+                        string split1 = word.Substring(0, vowelPlace);
+                        string split2 = word.Substring(vowelPlace);
+                        Console.WriteLine(split2 + split1 + "ay");
+                    }
                 }
-                else
-                {
-                    goAgain = false;
+                    Console.WriteLine("Would you like to translate another word?(y/n)");
+                    char answer = char.Parse(Console.ReadLine());
+                    if (char.ToLower(answer) == 'y')
+                    {
+                        goAgain = true;
+                    }
+                    else
+                    {
+                        goAgain = false;
+                    }
                 }
-            }
+            
             while (goAgain == true);
         }
 
