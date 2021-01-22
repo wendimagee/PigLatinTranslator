d0d60f485db52fa21d5fc000a147ebb4d2ecb2de
Wendi Magee <wendimagee@gmail.com>
2021-01-22 09:27:12 -0500

    I think I got it! Will spend the next few days to make the code work with sentences and not just words

--- PigLatinTranslator/Program.cs
+++ PigLatinTranslator/Program.cs
@@ -7,21 +7,55 @@ namespace PigLatinTranslator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello and welcome to the Pig Latin Translator! Please enter a word");
-            string input = Console.ReadLine();
-            Console.WriteLine(input.Substring(0,1));
-            if(input.Substring(0,1) == "a" || input.Substring(0, 1) == "e" || input.Substring(0, 1) == "o" || input.Substring(0, 1) == "u" || input.Substring(0, 1) == "i")
-            {
-                Console.WriteLine(input + "way");
-            }
-            else
-            {
-                Console.WriteLine("yikes");
-            }
-        }
-        
+            bool goAgain = true;
 
-    
-    
+            do
+            {
+
+
+                Console.WriteLine("Hello and Welcome to the Pig Latin Translator!");
+                Console.WriteLine("\nPlease enter a word: ");
+                string input = Console.ReadLine();
+
+                // make an array of vowels to search for
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+                //test input for vowels, find out where they are
+                int vowelPlace = input.IndexOfAny(vowels);
+
+                if (vowelPlace == 0)
+                {
+                    Console.WriteLine(input + "way");
+                }
+                //if the word doesn't begin with a vowel
+                else if (vowelPlace == 1)
+                {
+
+                    string split1 = input.Substring(0, vowelPlace);
+                    string split2 = input.Substring(vowelPlace);
+                    Console.WriteLine(split2 + split1 + "ay");
+                }
+                else
+                {
+                    string split1 = input.Substring(0, vowelPlace);
+                    string split2 = input.Substring(vowelPlace);
+                    Console.WriteLine(split2 + split1 + "ay");
+                }
+
+                Console.WriteLine("Would you like to translate another word?(y/n)");
+                char answer = char.Parse(Console.ReadLine());
+                if (char.ToLower(answer) == 'y')
+                {
+                    goAgain = true;
+                }
+                else
+                {
+                    goAgain = false;
+                }
+            }
+            while (goAgain == true);
+        }
+
     }
 }
+
