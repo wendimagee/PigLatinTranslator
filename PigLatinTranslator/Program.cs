97d75698f57f2fb1f7ac7db7ffe348a353a52d83
Wendi Magee <wendimagee@gmail.com>
2021-04-05 10:07:58 -0400

    I finally adjusted this translator to accept words like "gym" that use only y as a vowel. I also added a try/catch for "would you like to continue" Readline

--- PigLatinTranslator/Program.cs
+++ PigLatinTranslator/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
-//this code works for every word except those that end in y and have no other consonants.
-//my apologies! I am learning. I used a try/catch block to handle this exception! Practicing is fun
+
 namespace PigLatinTranslator
 {
     class Program
@@ -10,21 +9,17 @@ namespace PigLatinTranslator
         {
             bool goAgain = true;
             Console.WriteLine("Hello and Welcome to the Pig Latin Translator!");
-
             do
             {
                 try
                 {
                     Console.WriteLine("\nPlease enter a word or sentence to translate: ");
-                    string input = Console.ReadLine();
-                    //split those words up!
+                    string input = Console.ReadLine().ToLower();
                     string[] words = input.Split(' ');
                     foreach (var word in words)
                     {
-                        // make an array of vowels to search for
                         char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
 
-                        //make an array of symbols to search for
                         char[] symbols = { '@', '0', '1', '2', '3', '4',
                         '5', '6', '7', '8', '9', '#', '$', '%', '&',
                         '~', '^', '*', '+', '='};
@@ -40,42 +35,57 @@ namespace PigLatinTranslator
                             Console.WriteLine("HEY! I said enter a word or sentence! Try again.");
                             break;
                         }
-
-                        else if (word.IndexOfAny(symbols) != -1)
+                        //words with y as a vowel
+                        else if (word.IndexOfAny(symbols) == -1)
                         {
-                            break;
-                        }
-
-                        else if (vowelPlace == 0)
-                        {
-                            Console.Write(word + "way ");
-                        }
-                        //if the word doesn't begin with a vowel
-
-                        else
-                        {
-                            string split4 = SplitEmUp(word, vowelPlace);
-                            Console.Write(split4);
+                            if (word.Contains('y'))
+                            {
+                                vowelPlace = word.IndexOf('y');
+                                string split4 = SplitEmUp(word, vowelPlace);
+                                Console.Write(split4);
+                            }
+                            //words beginning with vowels:
+                            else if (vowelPlace == 0)
+                            {
+                                Console.Write(word + "way ");
+                            }
+                            //words that don't begin with vowels:
+                            else if (vowelPlace > 0)
+                            {
+                                string split4 = SplitEmUp(word, vowelPlace);
+                                Console.Write(split4);
+                            }
+                            else
+                            {
+                                throw new Exception();
+                            }
                         }
                     }
                 }
                 catch
                 {
-                    Console.WriteLine("\nI’m sorry, my translator does not accept words where the letter y is used as a vowel.Try a different word or sentence!");
+                    Console.WriteLine("\nWe cannot detect an English word or sentence. Try a different word or sentence!");
                 }
-            
-            
 
-                Console.WriteLine("\n\nWould you like to translate another word?(y/n)");
-                char answer = char.Parse(Console.ReadLine());
-                if (char.ToLower(answer) == 'y')
+                try
                 {
-                    goAgain = true;
+                    Console.WriteLine("\n\nWould you like to translate another word?(y/n)");
+                    char answer = char.Parse(Console.ReadLine());
+                    if (char.ToLower(answer) == 'y')
+                    {
+                        goAgain = true;
+                    }
+                    else
+                    {
+                        goAgain = false;
+                    }
                 }
-                else
+                catch
                 {
-                    goAgain = false;
+                    Console.WriteLine("\nYou must enter a y or n");
+
                 }
+
             }
 
             while (goAgain == true);
@@ -83,11 +93,9 @@ namespace PigLatinTranslator
 
         public static string SplitEmUp(string word, int vowelPlace)
         {
-           
                 string split1 = word.Substring(0, vowelPlace);
                 string split2 = word.Substring(vowelPlace);
                 return (split2 + split1 + "ay ");
-            
         }
     }
 }
